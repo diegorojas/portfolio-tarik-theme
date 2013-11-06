c463e27cd8a439a10dc73ee42aa6a140b1e06728
unknown <diego@brasa.art.br>
2013-11-06 20:17:38 -0200

    limpeza no function

--- functions.php
+++ functions.php
@@ -4,59 +4,7 @@ if ( function_exists( 'add_image_size' ) ) {
     add_image_size( 'galeria', 500, 500, true );
 }
 
-
-// Função do Carousel na Unha.
-function load_caroufredsel() {
-	$path_theme = get_bloginfo('stylesheet_directory');
-    wp_register_script( 'caroufredsel', $path_theme . '/js/jquery.carouFredSel-6.1.0-packed.js', array( 'jquery' ), '6.1.0', true );
-    wp_enqueue_script( 'caroufredsel_pre', $path_theme . '/js/caroufredsel_pre.js', array( 'caroufredsel' ), '', true );
-    wp_enqueue_script( 'custom-hover', $path_theme . '/js/custom-hover.js');
-}
-add_action( 'wp_enqueue_scripts', 'load_caroufredsel' );
-
-
-// Função para usar o Thickbox nativo do WP no tema Portfolio Brasa
-function add_themescript(){
-    if(!is_admin()){
-    wp_enqueue_script('jquery');
-    wp_enqueue_script('thickbox',null,array('jquery'));
-    wp_enqueue_style('thickbox.css', '/'.WPINC.'/js/thickbox/thickbox.css', null, '1.0');
-    }
-}
-add_action('init','add_themescript');
-
-// Insere os metaboxes (MetaBrasa) no CPT Portolio
-// require_once (get_stylesheet_directory() . '/metaboxes-portfolio.php');
-
-
-	// Add support for custom headers.
-
-	$custom_header_support = array(
-
-		// The default header text color.
-
-		'default-text-color' => '000',
-
-		// The height and width of our custom header.
-
-		'width' => apply_filters( 'twentyeleven_header_image_width', 1000 ),
-
-		'height' => apply_filters( 'twentyeleven_header_image_height', 288 ),
-
-		// Support flexible heights.
-
-		'flex-height' => true,
-
-		// Random image rotation by default.
-
-		'random-default' => true,
-	);
-
-
-
-	add_theme_support( 'custom-header', $custom_header_support );
-
-
+add_theme_support( 'custom-header', $custom_header_support );
 
 	if ( ! function_exists( 'get_custom_header' ) ) {
 
@@ -75,7 +23,5 @@ add_action('init','add_themescript');
 		add_custom_background();
 
 	}
-
-
 
 ?>
